7a5c7f16b0142409b3a23ea03baf3fd581a9da07
subhro7264 <subhrohazra7.com>
2023-09-25 13:27:56 +0530

    Add to Cart Fixes

--- advance-redux/src/App.js
+++ advance-redux/src/App.js
@@ -1,11 +1,12 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-
+import { useSelector } from 'react-redux';
 function App() {
+  const isVisible= useSelector((state) => state.ui.cart);
   return (
     <Layout>
-      <Cart />
+    { isVisible&& <Cart />}
       <Products />
     </Layout>
   );
